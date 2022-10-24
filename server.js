49c798edf90e9f54a63dc69981e93f236acadf22
vpratnicki <vanessa@pratnicki.net>
2022-10-24 19:40:34 -0400

    Initial file setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
