f2db58d1f8239fac5c4f7169dadfb48be72b525e
vpratnicki <vanessa@pratnicki.net>
2022-10-24 21:24:24 -0400

    Chore: created home-routes and connected to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-// const routes = require('./controllers/');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
