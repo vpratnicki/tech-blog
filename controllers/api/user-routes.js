6161a448232825398d6b4cdc2e3fedc48f924252
vpratnicki <vanessa@pratnicki.net>
2022-10-26 18:19:43 -0400

    Test: All user routes working correctly

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,6 +60,27 @@ router.post("/", (req, res) => {
     });
 });
 
+// PUT request
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 router.delete('/:id', (req, res) => {
     User.destroy({
       where: {
