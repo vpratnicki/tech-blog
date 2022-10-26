f56ae6b64e97345cc59ef89220d0ed363580f1ce
vpratnicki <vanessa@pratnicki.net>
2022-10-25 21:10:09 -0400

    Chore: Created home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,40 @@
 const router = require('express').Router();
-
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
-});
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // pass a single object into the homepage temp
+        console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
 
+        res.render('homepage');
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
 
 module.exports = router;
