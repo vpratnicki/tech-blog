f709dfc2335dbfb68572b32cdb53ed518897dfa6
vpratnicki <vanessa@pratnicki.net>
2022-10-29 13:40:06 -0400

    Chore: adjusted views based on login status

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
         console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+         });
     })
     .catch(err => {
         console.log(err);
@@ -70,7 +73,10 @@ router.get('/post/:id', (req, res) => {
           const post = dbPostData.get({ plain: true });
     
           // pass data to template
-          res.render('single-post', { post });
+          res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn
+           });
         })
         .catch(err => {
           console.log(err);
