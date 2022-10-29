263254d0ec571a073bf83ebbb46dd16ff69e8b56
vpratnicki <vanessa@pratnicki.net>
2022-10-29 18:30:06 -0400

    Chore: added helper functions to handlebars

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
