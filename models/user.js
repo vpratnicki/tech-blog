40d819f3c633b2acabae0a594114bf2c62812307
vpratnicki <vanessa@pratnicki.net>
2022-10-30 22:07:31 -0400

    updated user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create user model
+// create the user model
 class User extends Model {
     // set up method to run on instance data (per user) to check password
     checkPassword(loginPw) {
